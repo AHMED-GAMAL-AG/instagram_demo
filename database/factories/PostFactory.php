14ec2cb5f28c859fdda8bc707152eea414b92311
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-01-26 08:47:47 +0200

    06. إعداد بيانات الاختبار للمنشورات

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'post_caption' => $this->faker->paragraph,
+            // the faker takes the place to store the image and return the image name so concatenate 'uploads/' before it to store in the database
+            'image_path' => 'uploads/' . $this->faker->image('public/storage/uploads', 800, 600, null, false),
+        ];
+    }
+}
