7e4037d1dfce54609d972f33b3a74a0e895b28ae
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-02-05 16:49:43 +0200

    01. إعداد بنية المشروع لإضافة الإعجابات

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Post extends Model
 {
@@ -28,5 +29,35 @@ class Post extends Model
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+
+    // post has many likes by many users
+    public function likedByUsers()
+    {
+        return $this->belongsToMany(User::class, 'likes');
+    }
+
+    public function like(User $user)
+    {
+        return $this->likedByUsers()->save($user); // save the user_id and post_id to the likes table
+    }
+
+    public function dislike(User $user)
+    {
+        return $this->likedByUsers()->detach($user); // remove the user_id and post_id from the likes table
+    }
+
+    public function likedByUser(User $user) // check if the post is liked by user
+    {
+        return (bool) DB::table('likes')
+            ->where('user_id', $user->id)
+            ->where('post_id', $this->id)
+            ->count();
+    }
+
+
+    public function likeSystem(User $user)
+    {
+        return $this->likedByUser($user) ?  $this->dislike($user) :  $this->like($user);
     }
 }
