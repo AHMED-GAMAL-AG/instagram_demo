677bdea1db914147f115c3a2d7fdc21e76c352ae
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-02-06 10:43:49 +0200

    01. إعداد المشروع لعمليات المتابعة

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,6 +64,28 @@ class User extends Authenticatable
 
     public function posts()
     {
-        return $this->hasMany(Post::class)->orderBy('created_at','DESC');
+        return $this->hasMany(Post::class)->orderBy('created_at', 'DESC');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function follows() // the user can follow many users
+    {
+        // the table that links the two models , the current foreign id , the other user foreign id
+        return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
+    }
+
+    public function followers() // the user can be followed by many users
+    {
+        // the table that links the two models , the current foreign id , the other user foreign id
+        return $this->belongsToMany(User::class, 'follows', 'following_user_id', 'user_id');
+    }
+
+    public function following(User $user) // to check if a user is following another one
+    {
+        return $this->follows()->where('following_user_id', $user->id)->exists(); // if this user follow the $user return true
     }
 }
